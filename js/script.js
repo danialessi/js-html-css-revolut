2fc42cfe8381ca3349f644e73a2445bb69ff47b0
Daniele Alessi <daniele.alessi@hotmail.it>
2021-04-09 18:07:56 +0200

    fixato dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
     
-    // assegno all'elemento l'evento per mostrare il dropdown-menu 
+    // evento mouseenter 
         $('.menu').mouseenter(function () {
             $(this).children('.dropdown-menu').show();
-    });
+        });
+
+        $('.menu-title').mouseenter(function () {
+            $(this).addClass('color-blue');
+        })
 
     // assegno all'elemento l'evento per nascondere il dropdown-menu
-    $('.menu').mouseleave(function () {
-        $(this).children('.dropdown-menu').hide();
-    });
+        $('.menu').mouseleave(function () {
+            $(this).children('.dropdown-menu').hide();
+        });
+
+        $('.menu-title').mouseleave(function () {
+            $(this).removeClass('color-blue');
+        })
+
+    // IT chevron 
+        
 });
