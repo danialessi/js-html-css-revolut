7dfcf00f01836c620e266c6b732845bf88727203
Daniele Alessi <daniele.alessi@hotmail.it>
2021-04-09 16:59:14 +0200

    aggiunti eventi show hide

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
     
+    // assegno all'elemento l'evento per mostrare il dropdown-menu 
+        $('.menu').mouseenter(function () {
+            $(this).children('.dropdown-menu').show();
+    });
+
+    // assegno all'elemento l'evento per nascondere il dropdown-menu
+    $('.menu').mouseleave(function () {
+        $(this).children('.dropdown-menu').hide();
+    });
 });
