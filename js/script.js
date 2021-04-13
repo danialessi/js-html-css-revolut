7e10d801615b9bdae970b9073925e12954128042
danialessi <daniele.alessi@hotmail.it>
2021-04-13 17:44:44 +0200

    aggiunto mousenter IT

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     
-    // evento mouseenter 
+    // evento mouseenter dropdown
         $('.menu').mouseenter(function () {
             $(this).children('.dropdown-menu').show();
         });
 
         $('.menu-title').mouseenter(function () {
             $(this).addClass('color-blue');
-        })
+        });
 
     // assegno all'elemento l'evento per nascondere il dropdown-menu
         $('.menu').mouseleave(function () {
@@ -18,6 +18,16 @@ $(document).ready(function () {
             $(this).removeClass('color-blue');
         })
 
-    // IT chevron 
-        
+    // language
+        $('.language').mouseenter(function () {
+            $(this).children('.fa-chevron-down').show();
+            $(this).children('.fa-chevron-up').hide();
+            // $(this).children('.dropdown-menu').show();
+        })
+
+        $('.language').mouseleave(function () {
+            $(this).children('.fa-chevron-down').hide();
+            $(this).children('.fa-chevron-up').show();
+            // $(this).children('.dropdown-menu').hide();
+        })
 });
